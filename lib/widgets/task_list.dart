1db5b8c6885db0e9151d42f9cc0610ab8210dafd
Monzir Abdalrahman <monzir3bdo@gmail.com>
2022-07-01 16:59:37 +0300

    first commit

---
+++ lib/widgets/task_list.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_app/widgets/task_tile.dart';
+
+import '../models/task_data.dart';
+
+class TasksList extends StatelessWidget {
+  const TasksList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<TaskData>(
+      builder: (context, taskData, child) {
+        return ListView.builder(
+          itemBuilder: (context, index) {
+            final task = taskData.tasks[index];
+            return TaskTile(
+              taskTitle: task.name,
+              isChecked: task.isDone,
+              checkboxCallback: (checkboxState) {
+                taskData.updateTask(task);
+              },
+              longPressCallback: () {
+                taskData.deleteTask(task);
+              },
+            );
+          },
+          itemCount: taskData.taskCount,
+        );
+      },
+    );
+  }
+}
